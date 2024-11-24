1c70d282687e46a6dfc272cc32d465feb4385f93
ktmouk <ktmouk@gmail.com>
2024-11-24 16:25:39 +0900

    chore: update nextra

---
+++ pages/_meta.ts
@@ -0,0 +1,14 @@
+const meta = {
+  index: {
+    display: "hidden",
+    theme: {
+      layout: "raw",
+    },
+  },
+  "get-started": "Get Started",
+  guide: "Guide",
+  "prisma-plugin": "Prisma Plugin",
+  "other-orms": "Other ORMs",
+};
+
+export default meta;
---
+++ pages/prisma-plugin/_meta.ts
@@ -0,0 +1,6 @@
+export const meta = {
+  "get-started": "Get Started",
+  "best-practice": "Best Practice",
+};
+
+export default meta;
